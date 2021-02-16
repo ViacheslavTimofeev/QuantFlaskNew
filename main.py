26728fd0971ca3832992d7445eec1e877cd7be66
ViacheslavTimofeev <v.t.e3016@gmail.com>
2021-02-16 19:52:13 +0700

    2 commit

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def student():
-   return render_template('student.html')
 
-@app.route('/result',methods = ['POST', 'GET'])
-def result():
-   if request.method == 'POST':
-      result = request.form
-      return render_template("result.html",result = result)
+@app.route('/', methods=['post', 'get'])
+def index():
+    if request.method == 'POST':
+        name = request.form.get('str')
+        fio = request.form.get('comment')
+        print(name)
+        print(fio)
+    return render_template("Index.html")
 
+
+name = """Иванов Иван Иванович
+         Пётр Петров
+      """
+print(name)
+name2 = name.split('\n')
+for fio in name2:
+    print(fio.rstrip())
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug=True)
