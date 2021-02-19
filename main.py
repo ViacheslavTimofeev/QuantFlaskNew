a5a5ddfe739de9bcc9e90c97515b232bd57b93a0
swompy <070zarybin070@gmail.com>
2021-02-19 12:53:08 +0700

    3 commit

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
+
+
 @app.route('/', methods=['post', 'get'])
 def index():
     if request.method == 'POST':
@@ -10,15 +12,13 @@ def index():
         fio = request.form.get('comment')
         print(name)
         print(fio)
+        name2 = name.split()
+        print(name2)
+        for fname in name2:
+            print(fname.rstrip())
     return render_template("Index.html")
 
 
-name = """Иванов Иван Иванович
-         Пётр Петров
-      """
-print(name)
-name2 = name.split('\n')
-for fio in name2:
-    print(fio.rstrip())
+
 if __name__ == '__main__':
     app.run(debug=True)
