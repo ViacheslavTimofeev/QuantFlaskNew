d36cbbe9391cc6d15dc2628b07a6291cf1c0804a
ViacheslavTimofeev <v.t.e3016@gmail.com>
2021-03-02 19:33:36 +0700

    big update

--- certificate.py
+++ certificate.py
@@ -7,15 +7,23 @@ from zipfile import ZipFile
 def certificate(name, fio, date1, date2, choose):
     f = fio.split("\r\n")
     print(f)
-    if choose == "ie":
+    zipObj = ZipFile('sample.zip', 'w')
+    if choose == "Base":
         for person in f:
-            doc = DocxTemplate("test_base.docx")
+            doc = DocxTemplate("Sertifikat_ryzhiiy2.docx")
             context = {'Program': name, 'Name': person.rstrip(), 'Date1': date1, 'Date2': date2, 'Choose': choose}
             doc.render(context)
             doc.save("{}_base.docx".format(person.rstrip()))
+            convert("{}_base.docx".format(person.rstrip()))
+            zipObj.write('{}_base.pdf'.format(person.rstrip()))
+
     else:
         for person in f:
-            doc = DocxTemplate("test_project.docx")
+            doc = DocxTemplate("Sertifikat_ryzhiiy2.docx")
             context = {'Program': name, 'Name': person.rstrip(), 'Date1': date1, 'Date2': date2, 'Choose': choose}
             doc.render(context)
             doc.save("{}_project.docx".format(person.rstrip()))
+            convert("{}_project.docx".format(person.rstrip()))
+            zipObj.write('{}_project.pdf'.format(person.rstrip()))
+
+    zipObj.close()
